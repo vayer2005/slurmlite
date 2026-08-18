05614993b21651f4bf8c6582a5ca28993821a86e
vayer2005 <vayer2005@gmail.com>
2026-08-17 20:39:17 -0700

    feat(cluster): add Registry and Node skeleton stubs

--- internal/cluster/registry.go
+++ internal/cluster/registry.go
@@ -18,3 +18,63 @@
 package cluster
 
 // TODO: implement Node type and Registry with thread-safe operations
+import (
+	"sync"
+	"time"
+)
+type Status string
+
+const (
+	StatusOnline Status = "online"
+	StatusOffline Status = "offline"
+)
+
+type Node struct {
+	ID string
+	Status Status
+	TotalCPUs int
+	AllocatedCPUs int
+	LastHeartbeat time.Time
+	CurrentJobID string // -1 if not busy
+}
+
+
+
+type Registry struct {
+	nodes map[string]Node
+	mu sync.RWMutex
+}
+
+
+func (r *Registry) RegisterNode(node Node) {
+
+	
+}
+
+func (r *Registry) UpdateNode(node Node) {
+
+}
+
+func (r *Registry) MarkNodeOffline(node Node) {
+
+}
+
+func (r *Registry) ListNodes() []Node {
+
+	return nil
+}	
+
+func (r *Registry) FindAvailableNodes(numNodes int) []Node {
+
+	return nil
+}
+
+func (r *Registry) ReserveNodes(nodeIDs []string) error {
+
+	return nil
+}
+
+func (r *Registry) ReleaseNodes(nodeIDs []string) error {
+
+	return nil
+}	
